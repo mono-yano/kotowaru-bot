d8a2c2f6475c57a535f3a489492a7c6065e6bb2f
mono-yano <yano@monosus.co.jp>
2023-01-05 11:54:02 +0900

    docs:messageの配列作成/vscode上のボタンの作成/ボタンクリックしたら選択肢を順番に表示/選択した項目に応じたmessageを指定の個数表示

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,160 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "kotowaru-bot" is now active!');
+	const refuseMessages = [
+		{
+			who: '目上',
+			what: '案件',
+			// 'how': '',
+			message: 'この度は、お声掛けいただき恐れ入ります。大変恐縮ではございますが、ご提案いただいた日時ですとスケジュールが合わないためお受けすることができません。もし調整が可能であれば、○月○日以降であればお受けすることができますので、一度ご検討いただけますでしょうか。'
+		},
+		{
+			who: '目上',
+			what: '案件',
+			// 'how': '',
+			message: '111111111111111111111'
+		},
+		{
+			who: '目上',
+			what: '案件',
+			// 'how': '',
+			message: '2222222222222222222222222'
+		},
+		{
+			who: '目上',
+			what: '案件',
+			// 'how': '',
+			message: '33333333333333333'
+		},
+		{
+			who: '目上',
+			what: '案件',
+			// 'how': '',
+			message: '444444444444444444444444444444444444444'
+		},
+		// {
+		// 	'who': '目上',
+		// 	'what': '案件',
+		// 	// 'how': '',
+		// 	'message': 'いつもお世話になっております。新たなお仕事のご依頼をいただきまして誠にありがとうございます。検討させていただきましたが、誠に残念ながらスケジュール面での折り合いがつかず、お引き受けすることができない状況です。ご依頼をいただいたのにもかかわらずお役に立てず申し訳ございません。今後またこのような機会がございましたらぜひご協力させていただきたいので、その際はお手数をおかけしますがご連絡をいただけますと幸いです。'
+		// },
+		{
+			who: '目上',
+			what: '飲み会',
+			// 'how': '丁寧',
+			message: 'このたびはお誘いくださいまして、誠にありがとうございます。せっかくのお誘いにも関わらず心苦しい限りではございますが、あいにく先約があり、今回は遠慮させて頂きます。また別の機会にご一緒させて頂ければと存じます。'
+		},
+		{
+			who: '同僚',
+			what: '案件',
+			// 'how': '丁寧',
+			message: 'この度は、素敵な案件にお声掛けいただきありがとうございます。内容、確認いたしました。誠に恐縮ではございますが、ご提示いただいた条件では、○○までの対応となってしまいます。'
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('kotowaru-bot.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from kotowaru-bot!');
-    });
+		},
+		{
+			who: '同僚',
+			what: '飲み会',
+			// 'how': '',
+			message: 'お声がけいただき、ありがとうございます。せっかくのお誘いですが、当日は別の予定が入っているため参加は遠慮させていただきます。また次の機会にどうぞよろしくお願いします。'
+		},
+		{
+			who: '同僚',
+			what: '飲み会',
+			// 'how': '',
+			message: 'ランダム確認用1'
+		},
+		{
+			who: '同僚',
+			what: '飲み会',
+			// 'how': '',
+			message: 'ランダム確認用2ランダム確認用2'
+		},
+		{
+			who: '同僚',
+			what: '飲み会',
+			// 'how': '',
+			message: 'ランダム確認用3ランダム確認用3ランダム確認用3'
+		}
+	];
 
-    context.subscriptions.push(disposable);
+	function getWebviewContent(randomIndex) {
+		// return refuseResult[randomIndex].message;
+		// return  console.dir(randomIndex);
+		return randomIndex;
+		// return `<!DOCTYPE html>
+		// <html lang="ja">
+		// 	<head>
+		// 		<meta charset="UTF-8">
+		// 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		// 	</head>
+		// 	<body>
+		// 		<ul id="messageList"></ul>
+		// 	</body>
+		// </html>`;
+	}
+
+	let result = vscode.commands.registerCommand('vscode-refuse.refuse', async() => {
+		const who = await vscode.window.showQuickPick(['目上', '同僚']);
+		const what = await vscode.window.showQuickPick(['案件', '飲み会']);
+		// const how = await vscode.window.showQuickPick(['丁寧', 'お笑い']);
+
+		let refuseResult = refuseMessages.filter(message => message.who === who).filter(message => message.what === what);
+
+		const array = refuseResult.flatMap(result => result.message);
+		const randomIndex = randomSelect(array.slice(), 2);
+
+		// for (var i = 0; i < randomIndex.length; i++) {
+		// 	var messageList = vscode.DocumentDropEdit.createElement('li');
+		// 	messageList.textContent = randomIndex[i];
+		// 	vscode.window.getElementById('list').appendChild(messageList);
+		// }
+
+		// 配列arrayからランダムにnumberの個数の要素を取り出す
+		function randomSelect(array: any[], number:number)
+		{
+			let newArray = [];
+			while(newArray.length < number && array.length > 0)
+			{
+				// 配列からランダムな要素を選ぶ
+				const random = Math.floor(Math.random() * array.length);
+				// 選んだ要素を別の配列に登録する
+				newArray.push(array[random]);
+				// もとの配列からは削除する
+				array.splice(random, 1);
+			}
+			return newArray;
+		}
+
+
+
+		if( what !== undefined) {
+			const panel = vscode.window.createWebviewPanel(
+				'kotowaru',
+				`${who}の方への${what}の断りメッセージは以下になります！`,
+				vscode.ViewColumn.One,
+				{}
+			);
+
+			panel.webview.html = getWebviewContent(randomIndex);
+		}
+
+	});
+
+	context.subscriptions.push(result);
+
+
+	// vscode右下にボタンを表示
+	const button = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Right,
+		0
+	);
+	button.command = 'vscode-refuse.refuse';
+	button.text = 'kotowaru 💬';
+	context.subscriptions.push(button);
+	button.show();
+
+
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
