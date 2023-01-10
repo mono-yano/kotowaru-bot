5a8067a284e77ad447be5ffd79e5792df118487e
mono-yano <yano@monosus.co.jp>
2023-01-10 14:58:37 +0900

    docs:データ更新

--- src/extension.ts
+++ src/extension.ts
@@ -3,79 +3,30 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
 	const refuseMessages = [
-		{
-			who: '目上',
-			what: '案件',
-			// 'how': '',
-			message: 'この度は、お声掛けいただき恐れ入ります。大変恐縮ではございますが、ご提案いただいた日時ですとスケジュールが合わないためお受けすることができません。もし調整が可能であれば、○月○日以降であればお受けすることができますので、一度ご検討いただけますでしょうか。'
-		},
-		{
-			who: '目上',
-			what: '案件',
-			// 'how': '',
-			message: '111111111111111111111'
-		},
-		{
-			who: '目上',
-			what: '案件',
-			// 'how': '',
-			message: '2222222222222222222222222'
-		},
-		{
-			who: '目上',
-			what: '案件',
-			// 'how': '',
-			message: '33333333333333333'
-		},
-		{
-			who: '目上',
-			what: '案件',
-			// 'how': '',
-			message: '444444444444444444444444444444444444444'
-		},
-		// {
-		// 	'who': '目上',
-		// 	'what': '案件',
-		// 	// 'how': '',
-		// 	'message': 'いつもお世話になっております。新たなお仕事のご依頼をいただきまして誠にありがとうございます。検討させていただきましたが、誠に残念ながらスケジュール面での折り合いがつかず、お引き受けすることができない状況です。ご依頼をいただいたのにもかかわらずお役に立てず申し訳ございません。今後またこのような機会がございましたらぜひご協力させていただきたいので、その際はお手数をおかけしますがご連絡をいただけますと幸いです。'
-		// },
-		{
-			who: '目上',
-			what: '飲み会',
-			// 'how': '丁寧',
-			message: 'このたびはお誘いくださいまして、誠にありがとうございます。せっかくのお誘いにも関わらず心苦しい限りではございますが、あいにく先約があり、今回は遠慮させて頂きます。また別の機会にご一緒させて頂ければと存じます。'
-		},
-		{
-			who: '同僚',
-			what: '案件',
-			// 'how': '丁寧',
-			message: 'この度は、素敵な案件にお声掛けいただきありがとうございます。内容、確認いたしました。誠に恐縮ではございますが、ご提示いただいた条件では、○○までの対応となってしまいます。'
-
-		},
-		{
-			who: '同僚',
-			what: '飲み会',
-			// 'how': '',
-			message: 'お声がけいただき、ありがとうございます。せっかくのお誘いですが、当日は別の予定が入っているため参加は遠慮させていただきます。また次の機会にどうぞよろしくお願いします。'
-		},
-		{
-			who: '同僚',
-			what: '飲み会',
-			// 'how': '',
-			message: 'ランダム確認用1'
-		},
-		{
-			who: '同僚',
-			what: '飲み会',
-			// 'how': '',
-			message: 'ランダム確認用2ランダム確認用2'
-		},
-		{
-			who: '同僚',
-			what: '飲み会',
-			// 'how': '',
-			message: 'ランダム確認用3ランダム確認用3ランダム確認用3'
-		}
+		{who:"目上",what:"案件",how:"丁寧",message:"申し訳ありませんが他案件の対応でつまっており、対応できません。"},
+		{who:"目上",what:"案件",how:"丁寧",message:"残念ながら既にスケジュールが埋まっており、対応が難しいです。またお声かけください。"},
+		{who:"目上",what:"案件",how:"丁寧",message:"この度は、素敵な案件にお声掛けいただきありがとうございます。内容、確認いたしました。誠に恐縮ではございますが、ご提示いただいた条件では、○○までの対応となってしまいます。"},
+		{who:"目上",what:"案件",how:"フランク",message:"その期間はリソース埋まっていて、無理そうです。またお願いいたします。"},
+		{who:"目上",what:"案件",how:"フランク",message:"その期間はリソース埋まっていて、無理そうです。またお願いいたします。"},
+		{who:"目上",what:"案件",how:"フランク",message:"その期間はリソース埋まっていて、無理そうです。またお願いいたします。"},
+		{who:"目上",what:"飲み会",how:"丁寧",message:"申し訳ございませんが私用がありまして、今回は参加を見送らせていただきます"},
+		{who:"目上",what:"飲み会",how:"丁寧",message:"ありがたいお話ではございますが、先約があり不参加とさせてください。ご期待に添えず申し訳ございません。"},
+		{who:"目上",what:"飲み会",how:"丁寧",message:"このたびはお誘いくださいまして、誠にありがとうございます。せっかくのお誘いにも関わらず心苦しい限りではございますが、あいにく先約があり、今回は遠慮させて頂きます。また別の機会にご一緒させて頂ければと存じます。"},
+		{who:"目上",what:"飲み会",how:"フランク",message:"その日は予定がありすみませんが欠席とさせてください。またお誘いお待ちしています。"},
+		{who:"目上",what:"飲み会",how:"フランク",message:"その日は予定がありすみませんが欠席とさせてください。またお誘いお待ちしています。"},
+		{who:"目上",what:"飲み会",how:"フランク",message:"その日は予定がありすみませんが欠席とさせてください。またお誘いお待ちしています。"},
+		{who:"同僚",what:"案件",how:"丁寧",message:"お手伝いしたいのですが、リソース的に厳しいです。もし、手が空いたらお声かけしますね。"},
+		{who:"同僚",what:"案件",how:"丁寧",message:"お手伝いしたいのですが、リソース的に厳しいです。もし、手が空いたらお声かけしますね。"},
+		{who:"同僚",what:"案件",how:"丁寧",message:"お手伝いしたいのですが、リソース的に厳しいです。もし、手が空いたらお声かけしますね。"},
+		{who:"同僚",what:"案件",how:"フランク",message:"ちょっとリソース埋まってる。ごめんなさい。"},
+		{who:"同僚",what:"案件",how:"フランク",message:"ちょっとリソース埋まってる。ごめんなさい。"},
+		{who:"同僚",what:"案件",how:"フランク",message:"ちょっとリソース埋まってる。ごめんなさい。"},
+		{who:"同僚",what:"飲み会",how:"丁寧",message:"誘ってくれてありがとうございます。案件が立て込んでおり、参加できないです。また声かけてください。"},
+		{who:"同僚",what:"飲み会",how:"丁寧",message:"誘ってくれてありがとうございます。案件が立て込んでおり、参加できないです。また声かけてください。"},
+		{who:"同僚",what:"飲み会",how:"丁寧",message:"誘ってくれてありがとうございます。案件が立て込んでおり、参加できないです。また声かけてください。"},
+		{who:"同僚",what:"飲み会",how:"フランク",message:"ごめん予定ありです。また誘ってください"},
+		{who:"同僚",what:"飲み会",how:"フランク",message:"ごめん予定ありです。また誘ってください"},
+		{who:"同僚",what:"飲み会",how:"フランク",message:"ごめん予定ありです。また誘ってください"}
 	];
 
 	function getWebviewContent(randomMessage: string[]) {
@@ -121,11 +72,11 @@ export function activate(context: vscode.ExtensionContext) {
 	let result = vscode.commands.registerCommand('vscode-refuse.refuse', async() => {
 		const who = await vscode.window.showQuickPick(['目上', '同僚']);
 		const what = await vscode.window.showQuickPick(['案件', '飲み会']);
-		// const how = await vscode.window.showQuickPick(['丁寧', 'お笑い']);
+		const how = await vscode.window.showQuickPick(['丁寧', 'フランク']);
 
-		let refuseResult = refuseMessages.filter(message => message.who === who).filter(message => message.what === what);
+		let refuseResult = refuseMessages.filter(message => message.who === who).filter(message => message.what === what).filter(message => message.how === how);
 
-		const array = refuseResult.flatMap(result => result.message);
+		const array = refuseResult.map(result => result.message);
 		const randomMessage = randomSelect(array.slice(), 3);
 
 		// 配列arrayからランダムにnumberの個数の要素を取り出す
